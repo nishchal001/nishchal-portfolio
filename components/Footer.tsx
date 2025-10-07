f7126a83a1899342e8ba05f846637f55d01ec3a9
nishchal001 <nishchalchotai25@gmail.com>
2025-10-07 21:48:09 +0530

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+  return (
+    <footer className="mt-12 text-center text-gray-400">
+      <p>© {new Date().getFullYear()} Nishchal Chotai — Implementation and Platform Engineer</p>
+    </footer>
+  )
+}
